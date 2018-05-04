01c5ae6da2d15aed04f17f2bb6db7e06e9739d5d
twifty <waldermort@gmail.com>
2018-05-04 17:29:37 +0800

    tweaked schema merge, included tests

--- spec/shared-config-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import SharedConfigView from '../lib/shared-config-view';
-
-describe('SharedConfigView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
